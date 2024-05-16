02da293ee5a41b9e7be92c3d532a8b5865ef2402
Kalle Kivilahti <kalle.kivilahti@gmail.com>
2024-05-17 00:15:47 +0300

    status

--- spotify.py
+++ spotify.py
@@ -4,9 +4,13 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import pygame
 import requests
 import tempfile
+import os
+from dotenv import load_dotenv
 
-client_id = '00a9521c5d544c4ea183a9a71b9c3541'
-client_secret = '4ba57571ed6945d8ac24dc36d549bd06'
+load_dotenv()
+
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
@@ -19,9 +23,10 @@ root = customtkinter.CTk()
 root.geometry("900x1000")
 
 frame = customtkinter.CTkScrollableFrame(root, orientation="vertical", width=800, height=900)
-frame.pack(pady=40)
+frame.pack(pady=40) 
 
 def get_recommendations(event=None):
+
 
     query = entry1.get()
     
